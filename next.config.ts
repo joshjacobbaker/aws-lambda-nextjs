e23ba4d6312e4aad4a7f16352158473f296f176b
Josh Baker <josh.jacob.baker@gmail.com>
2025-02-08 23:26:59 -0800

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: "standalone",
+  experimental: {
+    appDir: true, // Required for App Router (Next.js 13+)
+  },
+  images: {
+    unoptimized: true, // AWS Lambda doesn't support automatic image optimization
+  },
 };
 
 export default nextConfig;
