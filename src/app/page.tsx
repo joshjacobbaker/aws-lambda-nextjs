86f8d3a18795aa372e405b70c48cfc37e11f0d09
Josh Baker <josh.jacob.baker@gmail.com>
2025-02-16 22:41:15 -0800

    added base url for images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 
+const BASE_URL = "http://aws-lambda-nextjs.s3-website-us-east-1.amazonaws.com";
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="http://aws-lambda-nextjs.s3-website-us-east-1.amazonaws.com/next.svg"
+          src={BASE_URL + "/next.svg"}
           alt="Next.js logo"
           width={180}
           height={38}
@@ -32,7 +34,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="http://aws-lambda-nextjs.s3-website-us-east-1.amazonaws.com/vercel.svg"
+              src={BASE_URL + "/vercel.svg"}
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -58,7 +60,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="http://aws-lambda-nextjs.s3-website-us-east-1.amazonaws.com/file.svg"
+            src={BASE_URL + "/file.svg"}
             alt="File icon"
             width={16}
             height={16}
@@ -73,7 +75,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="http://aws-lambda-nextjs.s3-website-us-east-1.amazonaws.com/window.svg"
+            src={BASE_URL + "/window.svg"}
             alt="Window icon"
             width={16}
             height={16}
@@ -88,7 +90,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="http://aws-lambda-nextjs.s3-website-us-east-1.amazonaws.com/globe.svg"
+            src={BASE_URL + "/globe.svg"}
             alt="Globe icon"
             width={16}
             height={16}
